8390e29c6a6fdb7e5649234e5e5234c519b29f78
earyzhikova <elenaryzh55@gmail.com>
2018-06-23 13:46:32 -0400

    Object in db

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -2,27 +2,16 @@ const mongoose = require("mongoose");
 const db = require("../models");
 mongoose.Promise = global.Promise;
 
-// This file empties the Books collection and inserts the books below
+// This file empties the Stories collection and inserts the stories below
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/stories",
-  {
-    useMongoClient: true
-  }
+  
 );
-[{
-  storyText: { type: String, required: true },
-  storyImgUrl: { type: String, required: true },
-  storyImgName: { type: String, required: true },
-  storyComments: 
-    [{
-      comments: { type: String, required: true },
-    }],
-  storyLikes: {type: Number}
 
 const storySeed = [
   {
-    storyText: "The Dead Zone",
+    storyText: "TCats",
     storyImgUrl: "https://giphy.com/",
     storyImgName: "CAT",
     storyComments: [
