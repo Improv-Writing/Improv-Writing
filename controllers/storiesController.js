0cd6c7723d1cab50794093cc9e7e105ac1cd4f6f
earyzhikova <elenaryzh55@gmail.com>
2018-06-23 08:35:21 -0400

    storiesController

--- controllers/storiesController.js
+++ controllers/storiesController.js
@@ -1,34 +1,34 @@
 const db = require("../models");
 
-// Defining methods for the booksController
+// Defining methods for the StoriesController
 module.exports = {
   findAll: function(req, res) {
-    db.Book
+    db.Stories
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book
+    db.Stories
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book
+    db.Stories
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book
+    db.Stories
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book
+    db.Stories
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
