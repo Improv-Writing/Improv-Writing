4efbf1927536580b9208f81cb1d9569bfc91b13b
earyzhikova <elenaryzh55@gmail.com>
2018-06-25 16:58:06 -0400

    StoryShare update

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,11 @@
 
 import React, { Component } from "react";
-import DeleteBtn from "../../components/DeleteBtn";
+// import DeleteBtn from "../components/DeleteBtn";
 // import Jumbotron from "../../components/Jumbotron";
 // import API from "../../utils/API";
 // import { Link } from "react-router-dom";
 import Example from "../components/Card/Test.js";
-// import CardProfile from "../components/Card/CardProfile"
+import CardProfile from "../components/Card/CardProfile"
 
 
 
@@ -17,10 +17,8 @@ class Profile extends Component {
     return (
       <div>
 
-     	<p>Profile</p>
-     	<Example />
-
-//      <CardProfile />
+   
+     	<CardProfile />
       </div>
     );
   }	
