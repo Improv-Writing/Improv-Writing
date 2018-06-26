baa648c7d97745e5fa29da345594c77e57ff10af
Thea Long <delong2@ncsu.edu>
2018-06-25 20:36:16 -0400

    updated buttons on Profile Page

--- client/src/components/Card/CardProfile.js
+++ client/src/components/Card/CardProfile.js
@@ -3,44 +3,76 @@ import { Card, Button, CardImg, CardHeader, CardBody,
   CardTitle, CardText, CardDeck, CardSubtitle } from 'reactstrap';
 // import { DeleteBtn } from "./DeleteBtn/DeleteBtn.js";
 // import ".CardProfile.css";
+// import DeleteBtn from "../DeleteBtn";
+import PropTypes from "prop-types";
 
 const CardProfile = (props) => {
   return (
-    <div>
-      <p className = "yourProfile"> Your Profile:</p>
     <CardDeck> 
       <Card body outline color="secondary">
-          {/* <CardTitle>Name</CardTitle> */}
         <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
         <CardBody>
-          <CardSubtitle>Title of the Story</CardSubtitle>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
           <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <Button>Share</Button>
-          { /* <DeleteBtn /> */ }
+          <CardText>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </CardText>
+          <Button color="primary">Share</Button>{' '}
+          <Button color="danger">Delete</Button>
+            { /* <DeleteBtn />*/ } 
         </CardBody>
       </Card>
       <Card body outline color="secondary">
-          {/* <CardTitle>Name</CardTitle> */}
         <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
         <CardBody>
-          <CardSubtitle>Title of the Story</CardSubtitle>
-          <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Share</Button>
-          {/* <Button>Button</Button> */}
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+          <CardText>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </CardText>
+          <Button color="primary">Share</Button>{' '}
+          <Button color="danger">Delete</Button>
+            { /* <DeleteBtn />*/ } 
         </CardBody>
       </Card>
       <Card body outline color="secondary">
-          {/* <CardTitle>Name</CardTitle> */}
         <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
         <CardBody>
-          <CardSubtitle>Title of the Story</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-          <Button>Share</Button>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+          <CardText>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </CardText>
+          <Button color="primary">Share</Button>{' '}
+          <Button color="danger">Delete</Button>
+            { /* <DeleteBtn />*/ } 
         </CardBody>
       </Card>
     </CardDeck>
-    </div>
   );
 };
+
+
+Button.propTypes = {
+  active: PropTypes.bool,
+  block: PropTypes.bool,
+  color: PropTypes.string, // default: 'secondary'
+  disabled: PropTypes.bool,
+
+  // Pass in a Component to override default button element
+  // example: react-router Link
+  // default: 'button'
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+
+  // ref will only get you a reference to the Button component, use innerRef to get a reference to the DOM element (for things like focus management).
+  innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+
+  onClick: PropTypes.func,
+  size: PropTypes.string
+}
+
 
 export default CardProfile;
