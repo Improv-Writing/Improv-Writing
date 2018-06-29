6d069748200134251b1312ff5cecc847d14f6a1f
DESKTOP-MOU56QV\Rhado <rhadorasrequiem@gmail.com>
2018-06-28 21:43:44 -0400

    fixed the counter for individual counts, still needs func to only work once though

--- client/src/components/Card/Counter.js
+++ client/src/components/Card/Counter.js
@@ -5,13 +5,41 @@ class CounterApp extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			count: 0
+			countLike: 0,
+			countLove: 0,
+			countLol: 0,
+			countScary: 0,
+			countSad: 0,
 		};
 	}
 
-	handleIncrement() {
+	handleIncrementLike() {
 		this.setState({
-			count: this.state.count + 1
+			countLike: this.state.countLike + 1
+		});
+	}
+
+	handleIncrementLove() {
+		this.setState({
+			countLove: this.state.countLove + 1
+		});
+	}
+
+	handleIncrementLol() {
+		this.setState({
+			countLol: this.state.countLol + 1
+		});
+	}
+
+	handleIncrementScary() {
+		this.setState({
+			countScary: this.state.countScary + 1
+		});
+	}
+
+	handleIncrementSad() {
+		this.setState({
+			countSad: this.state.countSad + 1
 		});
 	}
 
@@ -19,25 +47,25 @@ class CounterApp extends React.Component {
 		return (
 			<div>
 				<div>
-					<button onClick={() => this.handleIncrement()} className = "buttonLike">
+					<button onClick={() => this.handleIncrementLike()} className = "buttonLike">
 						<span role="img">👍</span>
-						<div id="counterLike">{this.state.count}</div>
+						<div id="counterLike">{this.state.countLike}</div>
 					</button>
-					<button onClick={() => this.handleIncrement()} className = "buttonLove">
+					<button onClick={() => this.handleIncrementLove()} className = "buttonLove">
 						<span role="img">😍</span>
-						<div id="conterLove">{this.state.count}</div>
+						<div id="conterLove">{this.state.countLove}</div>
 					</button>
-					<button onClick={() => this.handleIncrement()} className = "buttonLol">
+					<button onClick={() => this.handleIncrementLol()} className = "buttonLol">
 						<span role="img">🤣</span>
-						<div id="counterLol">{this.state.count}</div>
+						<div id="counterLol">{this.state.countLol}</div>
 					</button>
-					<button onClick={() => this.handleIncrement()} className = "buttonScary">
+					<button onClick={() => this.handleIncrementScary()} className = "buttonScary">
 						<span role="img">😨</span>
-						<div id="counterScary">{this.state.count}</div>
+						<div id="counterScary">{this.state.countScary}</div>
 					</button>
-					<button onClick={() => this.handleIncrement()} className = "buttonSad">
+					<button onClick={() => this.handleIncrementSad()} className = "buttonSad">
 						<span role="img">😭</span>
-						<div id="counterSad">{this.state.count}</div>
+						<div id="counterSad">{this.state.countSad}</div>
 					</button>
 				</div>
 			</div>
