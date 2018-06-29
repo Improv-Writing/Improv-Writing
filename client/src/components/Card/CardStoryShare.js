38bdd3f686e072816e0d2007630dce42aeea230b
DESKTOP-MOU56QV\Rhado <rhadorasrequiem@gmail.com>
2018-06-28 22:26:33 -0400

    dummy data

--- client/src/components/Card/CardStoryShare.js
+++ client/src/components/Card/CardStoryShare.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, Button, CardImg, CardHeader, CardBody,
   CardTitle, CardText, CardDeck, CardSubtitle } from 'reactstrap';
 //  import DeleteBtn from "../DeleteBtn";
+//https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap"
 import "./CardStoryShare.css";
 import emoji from 'react-easy-emoji';
 import Counter from './Counter.js';
@@ -11,8 +12,8 @@ const CardStoryShare = (props) => {
 
     <CardDeck style={{marginTop: 20,}}> 
       <Card body outline color="secondary" >
-          <CardTitle style= {{fontFamily: "Paytone One, sans-serif", color: "steelBlue", fontSize: 25, textShadow: "2px 2px 6px black",}}>Author:</CardTitle>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+          <CardTitle style= {{fontFamily: "Paytone One, sans-serif", color: "steelBlue", fontSize: 25, textShadow: "2px 2px 6px black",}}>Author: Terra_Angel1234</CardTitle>
+        <CardImg top width="100%" src="https://media0.giphy.com/media/pB6Nfy3n2Z2Zq/giphy.gif" alt="Card image cap" />
         <CardBody >
           <CardSubtitle  style={{fontFamily: "Payton One, sans-serif", color: "steelBlue", fontSize: 20, fontWeight: "bold",}}>Title of the Story</CardSubtitle>
           <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
@@ -21,8 +22,8 @@ const CardStoryShare = (props) => {
         </CardBody>
       </Card>
       <Card body outline color="secondary">
-          <CardTitle style= {{fontFamily: "Paytone One, sans-serif", color: "steelBlue", fontSize: 25, textShadow: "2px 2px 6px black",}}>Author:</CardTitle>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+          <CardTitle style= {{fontFamily: "Paytone One, sans-serif", color: "steelBlue", fontSize: 25, textShadow: "2px 2px 6px black",}}>Author: Drakeamus_Prime</CardTitle>
+        <CardImg top width="100%" src="https://media1.giphy.com/media/eenzqB2MsGKbK/giphy.gif" alt="Card image cap" />
         <CardBody>
           <CardSubtitle style={{fontFamily: "Payton One, sans-serif", color: "steelBlue", fontSize: 20, fontWeight: "bold",}}>Title of the Story</CardSubtitle>
           <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
@@ -30,8 +31,8 @@ const CardStoryShare = (props) => {
           </CardBody>
       </Card>
       <Card body outline color="secondary">
-          <CardTitle style= {{fontFamily: "Paytone One, sans-serif", color: "steelBlue", fontSize: 25, textShadow: "2px 2px 6px black",}}>Author:</CardTitle>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+          <CardTitle style= {{fontFamily: "Paytone One, sans-serif", color: "steelBlue", fontSize: 25, textShadow: "2px 2px 6px black",}}>Author: InHisYouth</CardTitle>
+        <CardImg top width="100%" src="https://media2.giphy.com/media/6T8wcUPfi8GDC/giphy.gif" alt="Card image cap" />
         <CardBody>
           <CardSubtitle style={{fontFamily: "Payton One, sans-serif", color: "steelBlue", fontSize: 20, fontWeight: "bold",}}>Title of the Story</CardSubtitle>
           <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
